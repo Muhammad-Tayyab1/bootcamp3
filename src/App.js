73572df51749e379ede75d0b37aeaf5fa1bb10cb
Muhammad-Tayyab1 <66192491+Muhammad-Tayyab1@users.noreply.github.com>
2020-06-10 10:22:54 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import Online_Shop from './Online Shop';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <Online_Shop ProductName="Home Appliances"MobName=" Mobile Phones" />
+   <Online_Shop cloth=" Clothes" books="Books"  />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Online Shop.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Online_Shop (props){
+    return (
+        <div> 
+
+    <h1>We are surving: {props.ProductName} {props.books}</h1><hr/>
+    <h1>We are also surving: {props.MobName} {props.cloth}</h1><hr/>
+       
+
+        </div>
+    );
+
+}
+export default Online_Shop;
