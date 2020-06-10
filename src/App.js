919e8246c4caec0cf466e6e4ebc87333fa90f64f
Muhammad-Tayyab1 <66192491+Muhammad-Tayyab1@users.noreply.github.com>
2020-06-10 12:03:39 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Online from './Online shop';
+import Online from './Onlineshop';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
    <Online ProductName="Home Appliances"MobName=" Mobile Phones" />
-   <Online cloth=" Clothes" books="Books"  />
     </div>
   );
 }
